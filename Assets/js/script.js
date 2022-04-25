9f2b7ce455a3638a2f8d479407dc733739bbf69a
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-04-25 23:06:09 +0100

    made an array for each password criteria

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -33,13 +33,84 @@ const getPasswordLength = () => {
   const passwordLength = prompt("Enter password length");
 
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("Please provide a password between 8 and 128 characters!");
+    alert("Please provide a number between 8 and 128 characters!");
     return null;
   }
   return passwordLength;
 };
 
 const getPasswordCriteria = () => {
+  const passwordCriteriaLowerCase = confirm(
+    "Would you like lowercase letters in your password?"
+  );
+  const passwordCriteriaUpperCase = confirm(
+    "would you like Uppercase letters in your password?"
+  );
+  const passwordCriteriaNumbers = confirm(
+    "would you like numbers is your password?"
+  );
+  const passwordCriteriaSpecialCharacter = confirm(
+    "Would you like any special characters in your password?"
+  );
+
+  const lowerCaseLetters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const uppercaseLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const specialCharecters = [" ","!","\",""","#","$","%","&","'","(","")"","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
   return [
     "abcdefghijklmnopqrstuvwxyz",
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
