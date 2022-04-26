681b20b1b0239ddc5cb3864d48f0eb103c83cdbc
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-04-26 23:52:29 +0100

    added a readme file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,104 +1,142 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
-
+const upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 const getPasswordLength = () => {
-  const passwordLength = prompt("Enter password length");
+  const passwordLength = parseInt(prompt("Enter password length"), 10);
 
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("Please provide a number between 8 and 128 characters!");
-    return null;
-  }
   return passwordLength;
 };
 
 const getPasswordCriteria = () => {
+  let passwordCriteria = [];
   const passwordCriteriaLowerCase = confirm(
     "Would you like lowercase letters in your password?"
   );
   // If the user presses ok (true) an array of lowercase letters is created.
-  if (true) {
-    const lowerCase = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ];
+  if (passwordCriteriaLowerCase) {
+    passwordCriteria = [...passwordCriteria, ...lowerCase];
   }
 
   const passwordCriteriaUpperCase = confirm(
     "would you like Uppercase letters in your password?"
   );
   // If the user presses ok (true) an array of uppercase letters is created.
-  if (true) {
-    const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  if (passwordCriteriaUpperCase) {
+    passwordCriteria = [...passwordCriteria, ...upperCase];
+  }
 
   const passwordCriteriaNumbers = confirm(
     "would you like numbers is your password?"
   );
-  // If the user presses ok (true) an array of lowercase letters is created.
-  if (true) {
-    const numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  // If the user presses ok (true) an array of lowercase letters is created.);
 
+  if (passwordCriteriaNumbers) {
+    passwordCriteria = [...passwordCriteria, ...numbers];
+  }
+
+  // If the user presses ok (true) an array of special characters is created.
   const passwordCriteriaSpecialCharacter = confirm(
     "Would you like any special characters in your password?"
   );
-
-
-  passwordCriteria = math.floor(math.random() * passwordLength);
-
-  return [
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "0123456789",
-    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
-  ];
+  if (passwordCriteriaSpecialCharacter) {
+    passwordCriteria = [...passwordCriteria, ...specialCharacters];
+  }
+  return passwordCriteria;
 };
 
-const createRandomPassword = () => {
-  return "kdUE28(@d0";
+//incomplete
+const createRandomPassword = (passwordLength, passwordCriteria) => {
+  console.log(passwordCriteria);
+  const password = [];
+  // for the password have a for loop
+  for (let i = 0; i < passwordLength; i++) {
+    // generate  the random index
+    passwordCriteria = [Math.floor(Math.random())];
+    //pick a random value from the array and push on the password array
+    password.push;
+    password;
+  }
+
+  return password.join("");
 };
 
 // main function to generate the random password
 const generatePassword = () => {
   // get the password length
   const passwordLength = getPasswordLength();
-  if (passwordLength === null) {
-    return null;
+  if (passwordLength > 8 && passwordLength < 128) {
+    // get the password criteria
+    const passwordCriteria = getPasswordCriteria();
+
+    // create random password
+    const password = createRandomPassword(passwordLength, passwordCriteria);
+
+    return password;
+  } else {
+    alert("please provide valid password length");
   }
-  // get the password criteria
-  const passwordCriteria = getPasswordCriteria();
-
-  // create random password
-  const password = createRandomPassword(passwordLength, passwordCriteria);
-
-  return password;
 };
 
 // Write password to the #password input
 const writePassword = () => {
-  const password = generatePassword();
+  const password = generatePassword() || "";
   if (password === null) {
     return;
   }
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # password-generator
+
+## DESCRIPTION
+
+This application displays a randomly generated password from the user input of how many characters they would like in their password and what characters they would like to include in the password. For example Lowercase, upper case, numbers and special characters.
+
+TODO
+
+- complete for loop to get random characters from password criteria array to occupy a password loop that can then be generated on the page.
+- add an alert to make sure the user has to pick at least one criteria
+  and an if statement to make sure the user is choosing at least one criteria.
+
+## Deployed URL
+
+You can find the application deployed to github pages [here](https://github.com/surajverma2587)
+
+## Technologies
+
+- HTML
+- CSS
+- GitHub Pages
+- Java script
+
+## Screenshots
+
+### Desktop Viewports
+
+![Webpage](./assets/passwordgenerator.jpg)
+
+### Mobile Viewports
+
+![spiderman](./assets/password%20generator%20mobile.jpg
